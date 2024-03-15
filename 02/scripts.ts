07dd31ce5c4235c75c32d592f15aec908cf10c1e
juan-bl <juannicolasdev@gmail.com>
2024-03-15 15:46:15 -0300

    Exercicio 02 concluido

--- 02/scripts.ts
+++ 02/scripts.ts
@@ -19,3 +19,21 @@ type Carrinho = {
     tipoTransacao: 'credito' | 'Debito',
     cartao: Cartao
 }
+
+type Endereco = {
+    cep: string,
+    rua: string,
+    bairro: string,
+    cidade: string,
+    estado: string
+}
+
+type NovoCarrinho = {
+    item: Item,
+    qtd: number,
+    desconto: number,
+    frete: number,
+    tipoTransacao: Lowercase<'credito' | 'debito'>,
+    cartao: Cartao,
+    endereco: Endereco
+}
