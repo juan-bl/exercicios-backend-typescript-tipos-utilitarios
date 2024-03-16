6067e35e4fe797b17ff848e4642c1089401303b0
juan-bl <juannicolasdev@gmail.com>
2024-03-16 15:45:56 -0300

    Exercicio 03 concluido

--- 03/scripts.ts
+++ 03/scripts.ts
@@ -6,10 +6,19 @@ type Conn = {
     dbname: string
 }
 
-const conexao = (dados: Conn) => {
+const conexao = (dados: Conn): Readonly<{driver: string, url: string}> => {
     const { username, password, host, port, dbname } = dados;
     return {
         driver: 'postgres',
         url: `postgresql://${username}:${password}@${host}:${port}/${dbname}`
     };
 }
+
+
+console.log(conexao({
+    username: 'Juan',
+    password: '0000',
+    host: '0000',
+    port: '0000',
+    dbname: 'Juan'
+}));
